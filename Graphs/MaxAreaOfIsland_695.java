b6bd116bbb5453acf28afef0f7cde6d2691a754e
Aman Kumar <arazz5349@gmail.com>
2026-02-15 22:55:13 +0530

    Solved MaxAreaOfIsland_695.java

---
+++ Graphs/MaxAreaOfIsland_695.java
@@ -0,0 +1,39 @@
+// LeetCode 695
+// Max Area of Island
+// Topic: Graphs (DFS on Grid)
+// Difficulty: Medium
+// Time Complexity: O(m*n)
+// Space Complexity: O(m*n)  // due to recursion stack in worst case
+
+class Solution {
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea=0;
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    maxArea=Math.max(maxArea,dfs(grid,i,j));
+                }
+            }
+        }
+
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid,int i,int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0)
+            return 0;
+
+        grid[i][j]=0;
+
+        int area=1;
+
+        area+=dfs(grid,i+1,j);
+        area+=dfs(grid,i-1,j);
+        area+=dfs(grid,i,j+1);
+        area+=dfs(grid,i,j-1);
+
+        return area;
+    }
+}
